5e67b1ff57713bfeeb4f097be20be11e8eb00b29
hdh <smoothiefrog@gmail.com>
2017-06-01 08:52:56 +0800

    web

---
+++ js/company.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+	$(".ui-main-imf-eve").on("mouseover", function(event) {
+		$(this).css("opacity", "0.6");
+	});
+	$(".ui-main-imf-eve").on("mouseout", function(event) {
+		$(this).css("opacity", "1");
+	});
+
+	$(".ui-main-top-text").on("mouseover", function(event) {
+		$(this).css("opacity", "0.6");
+	});
+	$(".ui-main-top-text").on("mouseout", function(event) {
+		$(this).css("opacity", "1");
+	});
+
+	
+	$(".ui-main-foot-left-text").on("mouseover", function(event) {
+		$(this).css("opacity", "0.6");
+	});
+	$(".ui-main-foot-left-text").on("mouseout", function(event) {
+		$(this).css("opacity", "1");
+	});
+
+	$(".ui-main-foot-right-text").on("mouseover", function(event) {
+		$(this).css("opacity", "0.6");
+	});
+	$(".ui-main-foot-right-text").on("mouseout", function(event) {
+		$(this).css("opacity", "1");
+	});
+	
+})
