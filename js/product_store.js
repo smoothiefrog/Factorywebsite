8dcad8602c17eca43ec1c2d39acdd538deaa80c3
hdh <smoothiefrog@gmail.com>
2017-06-08 22:56:33 +0800

    css style changed

--- js/product_store.js
+++ js/product_store.js
@@ -7,25 +7,25 @@
 $(document).ready(function(){
 $(init);
 
-function init() {  
-        // 向下拉保持title2浮在顶部
+function init() { 
+        // 向下拉保持fixed_title浮在顶部
     $(window).scroll(function(){
         var slidery=$(window).scrollTop();
         if(slidery>50)
         {
                 $(".title1").hide();
-                $(".title2").css({"top":"0","position":"fixed"});
+                $(".fixed_title").css({"top":"0","position":"fixed"});
 
         }
         else 
         {
                 $(".title1").show();
-                $(".title2").css({"top":"50px","position":"fixed"});
+                $(".fixed_title").css({"top":"50px","position":"fixed"});
         }
     })
     // 点击“产品商城”回到顶部
     var timer = null;
-    $(".title2-left").click(function(){
+    $(".fixed_title-left").click(function(){
         cancelAnimationFrame(timer);
         timer = requestAnimationFrame(function fn(){
           var toTop = document.body.scrollTop || document.documentElement.scrollTop;
